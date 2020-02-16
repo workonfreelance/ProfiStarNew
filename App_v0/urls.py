87f644f36df8263a17358671a63151f9fb4f5dee
niktarin <spainszlo@gmail.com>
2020-02-16 17:34:13 +0200

    update

--- App_v0/urls.py
+++ App_v0/urls.py
@@ -19,28 +19,8 @@ from App_v0.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "employ"
+app_name = "app_v0"
 
 urlpatterns = [
     path('', Start, name="start"),
-    # path('', start, name="start"),
-    # path('registration', registration, name="registration"),
-    # path('login',loginHTML,name="login"),
-    # path('action/<str:link>', user_activ, name="action"),
-    # path('user_info', user_info, name="user_info"),
-    # path('updata_user_info', updata_user_info, name="updata_user_info"),
-    # path('user_login', user_login, name="user_login"),
-    # path('add_comment', add_comment, name="add_comment"),
-    # path('', start, name="start"),
-    # path('save_form', save_form, name="save_form"),
-    # # path('<str:html_name>', vity_html, name="vity_html")
-    # path('login_out', login_out, name="login_out"),
-    # path('login_or',login_or,name="login_or"),
-    # path('user_login',user_login,name="user_login"),
-    # path('index', index, name="index"),
-    # path('deteil/<str:link>', deteil, name="deteil"),
-
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
